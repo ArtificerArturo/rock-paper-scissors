54ff794e5599f2f7555e4f67f692279e23068a00
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-01 16:02:48 -0600

    formatted better

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function randomInt(max) {
-    return Math.floor(Math.random() * max)+1
+    return Math.floor(Math.random() * max) + 1
 }
 
 function getComputerChoice() {
-    switch(randomInt(3)) {
+    switch (randomInt(3)) {
         case 1:
             computerChoice = "rock"
             break
         case 2:
             computerChoice = "paper"
             break
-        case 3: 
-        computerChoice = "scissors"
+        case 3:
+            computerChoice = "scissors"
     }
     return computerChoice
 }
 
 function getHumanChoice() {
     let humanChoice = prompt("Enter your RPS selection").toLowerCase()
-    if(['rock','paper','scissors'].includes(humanChoice)) {
+    if (['rock', 'paper', 'scissors'].includes(humanChoice)) {
         return humanChoice
     } else {
         console.log("bad input")
@@ -26,15 +26,15 @@ function getHumanChoice() {
 }
 
 function playGame() {
-    let humanScore=0
-    let computerScore=0
+    let humanScore = 0
+    let computerScore = 0
 
     function playRound(humanChoice, computerChoice) {
-        switch(humanChoice) {
+        switch (humanChoice) {
             case "rock":
-                if(computerChoice=="rock") {
-                    console.log("Tie")                
-                } else if(computerChoice=="paper") {
+                if (computerChoice == "rock") {
+                    console.log("Tie")
+                } else if (computerChoice == "paper") {
                     console.log("You lose")
                     computerScore += 1
                 } else {
@@ -43,46 +43,46 @@ function playGame() {
                 }
                 break
             case "paper":
-                if(computerChoice=="rock") {
-                    console.log("You win") 
-                    humanScore += 1               
-                } else if(computerChoice=="paper") {
-                    console.log("Tie") 
+                if (computerChoice == "rock") {
+                    console.log("You win")
+                    humanScore += 1
+                } else if (computerChoice == "paper") {
+                    console.log("Tie")
                 } else {
                     console.log("You lose")
                     computerScore += 1
                 }
-                break      
-                    case "paper":
-                if(computerChoice=="rock") {
-                    console.log("You win") 
-                    humanScore += 1               
-                } else if(computerChoice=="paper") {
-                    console.log("Tie") 
+                break
+            case "paper":
+                if (computerChoice == "rock") {
+                    console.log("You win")
+                    humanScore += 1
+                } else if (computerChoice == "paper") {
+                    console.log("Tie")
                 } else {
                     console.log("You lose")
                     computerScore += 1
                 }
-                break   
-                case "scissors":
-                    if(computerChoice=="rock") {
-                        console.log("You lose")
-                        computerScore += 1             
-                    } else if(computerChoice=="paper") {
-                        console.log("You win") 
-                        humanScore += 1   
-                    } else {
-                        console.log("Tie") 
-                    }
-                                        
+                break
+            case "scissors":
+                if (computerChoice == "rock") {
+                    console.log("You lose")
+                    computerScore += 1
+                } else if (computerChoice == "paper") {
+                    console.log("You win")
+                    humanScore += 1
+                } else {
+                    console.log("Tie")
+                }
+
         }
     }
 
-    for (i=0;i<5;i++) {
-        playRound(getHumanChoice(),getComputerChoice())
+    for (i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice())
     }
     console.log("Game over")
-    console.log("human score ",humanScore)
+    console.log("human score ", humanScore)
     console.log("computer score", computerScore)
 }
 
